e341d7005039f0f450e1f0222f02b924f47ce6b5
Ronit Kumar Maity <maity.ronit18@gmail.com>
2024-10-20 01:07:00 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd /home/ec2-user/myapp/
+set -e  # Exit on error
+set -x  # Enable debugging
+
+# Create the target directory if it doesn't exist
+mkdir -p /home/ec2-user/myapp
+
+# Change to the app directory
+cd /home/ec2-user/myapp
+
+# Install dependencies
 npm install --production
